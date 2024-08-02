026e8ea21b465f437d273aa05294c51b03ee5408
sia <luosianao@gmail.com>
2024-08-02 16:50:44 +0800

    add env

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const core = require("@actions/core");
+const { Env } = require("@humanwhocodes/env");
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
   try {
+    const Header =new Headers();
+    const raw =JSON.stringify({
+        "msg_type":"text",
+        "context":{
+            "text":"新更新提醒"
+        }
+    })
+    const reqOption={
+        method:"POST",
+        head:Header,
+        body:raw,
+    }
+    let hook=Env.get("LARK_WEBHOOK")
+    fetch(hook,reqOption)
     const state = core.getInput("state", { required: true });
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Run with ${state}`);
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message);
