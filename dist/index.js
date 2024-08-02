a0bc7f37205f1624ee55df4525eb437612688f52
sia <luosianao@gmail.com>
2024-08-02 18:11:53 +0800

    add json content type

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,10 @@ const { Env } = __nccwpck_require__(441);
  */
 async function run() {
   try {
+    const env = new Env();
     const Header =new Headers();
+    var card=template
+    card.card.elements[0].elements[0].content="触发者："+env.get("github.event_name")
     Header.append("Content-Type","application/json")
         const raw = JSON.stringify({
         "msg_type": "interactive",
@@ -26,7 +29,7 @@ const requestOptions = {
   redirect: "follow"
 };
 
-const env = new Env();
+
 let hook =env.get("LARK_WEBHOOK")
 
   fetch(hook, requestOptions)
@@ -48,210 +51,178 @@ module.exports = {
   run,
 };
 
-
 const template=
     {
-        "name":"Github action",
-        "dsl":{
-           "config":{
-              
-           },
-           "i18n_elements":{
-              "zh_cn":[
-                 {
-                    "tag":"column_set",
-                    "flex_mode":"none",
-                    "background_style":"default",
-                    "horizontal_spacing":"8px",
-                    "horizontal_align":"left",
-                    "columns":[
-                       {
-                          "tag":"column",
-                          "width":"weighted",
-                          "vertical_align":"top",
-                          "vertical_spacing":"2px",
-                          "background_style":"default",
-                          "elements":[
-                             {
-                                "tag":"markdown",
-                                "content":"**申请人：**",
-                                "text_align":"left",
-                                "text_size":"normal",
-                                "icon":{
-                                   "tag":"standard_icon",
-                                   "token":"assigned_outlined"
-                                }
-                             },
-                             {
-                                "tag":"column_set",
-                                "flex_mode":"none",
-                                "background_style":"default",
-                                "horizontal_spacing":"8px",
-                                "horizontal_align":"left",
-                                "columns":[
-                                   {
-                                      "tag":"column",
-                                      "width":"18px",
-                                      "vertical_align":"top",
-                                      "vertical_spacing":"8px",
-                                      "background_style":"default",
-                                      "elements":[
-                                         
-                                      ]
-                                   },
-                                   {
-                                      "tag":"column",
-                                      "width":"weighted",
-                                      "vertical_align":"top",
-                                      "vertical_spacing":"8px",
-                                      "background_style":"default",
-                                      "elements":[
-                                         {
-                                            "tag":"person_list",
-                                            "persons":[
-                                               {
-                                                  "id":"39af6166"
-                                               }
+        "msg_type": "interactive",
+        "card": {
+            "elements": [
+                {
+                    "tag": "column_set",
+                    "flex_mode": "none",
+                    "background_style": "default",
+                    "horizontal_spacing": "8px",
+                    "horizontal_align": "left",
+                    "columns": [
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "vertical_align": "top",
+                            "vertical_spacing": "2px",
+                            "background_style": "default",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "",
+                                    "text_align": "left",
+                                    "text_size": "normal",
+                                    "icon": {
+                                        "tag": "standard_icon",
+                                        "token": "assigned_outlined"
+                                    }
+                                },
+                                {
+                                    "tag": "column_set",
+                                    "flex_mode": "none",
+                                    "background_style": "default",
+                                    "horizontal_spacing": "8px",
+                                    "horizontal_align": "left",
+                                    "columns": [
+                                        {
+                                            "tag": "column",
+                                            "width": "18px",
+                                            "vertical_align": "top",
+                                            "vertical_spacing": "8px",
+                                            "background_style": "default",
+                                            "elements": []
+                                        },
+                                        {
+                                            "tag": "column",
+                                            "width": "weighted",
+                                            "vertical_align": "top",
+                                            "vertical_spacing": "8px",
+                                            "background_style": "default",
+                                            "elements": [
+                                                {
+                                                    "tag": "person_list",
+                                                    "persons": [
+                                                        {
+                                                            "id": "39af6166"
+                                                        }
+                                                    ],
+                                                    "size": "small",
+                                                    "show_avatar": true,
+                                                    "show_name": true
+                                                }
                                             ],
-                                            "size":"small",
-                                            "show_avatar":true,
-                                            "show_name":true
-                                         }
-                                      ],
-                                      "weight":1
-                                   }
-                                ],
-                                "margin":"0px 0px 0px 0px"
-                             }
-                          ],
-                          "weight":1
-                       },
-                       {
-                          "tag":"column",
-                          "width":"weighted",
-                          "vertical_align":"top",
-                          "vertical_spacing":"8px",
-                          "background_style":"default",
-                          "elements":[
-                             {
-                                "tag":"markdown",
-                                "content":"**休假类型：**\n年假",
-                                "text_align":"left",
-                                "text_size":"normal",
-                                "icon":{
-                                   "tag":"standard_icon",
-                                   "token":"label-change_outlined"
+                                            "weight": 1
+                                        }
+                                    ],
+                                    "margin": "0px 0px 0px 0px"
                                 }
-                             }
-                          ],
-                          "weight":1
-                       }
+                            ],
+                            "weight": 1
+                        },
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "vertical_align": "top",
+                            "vertical_spacing": "8px",
+                            "background_style": "default",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "**休假类型：**\n年假",
+                                    "text_align": "left",
+                                    "text_size": "normal",
+                                    "icon": {
+                                        "tag": "standard_icon",
+                                        "token": "label-change_outlined"
+                                    }
+                                }
+                            ],
+                            "weight": 1
+                        }
                     ],
-                    "margin":"16px 0px 0px 0px"
-                 },
-                 {
-                    "tag":"column_set",
-                    "flex_mode":"stretch",
-                    "background_style":"default",
-                    "horizontal_spacing":"8px",
-                    "horizontal_align":"left",
-                    "columns":[
-                       {
-                          "tag":"column",
-                          "width":"weighted",
-                          "vertical_align":"top",
-                          "vertical_spacing":"8px",
-                          "background_style":"default",
-                          "elements":[
-                             {
-                                "tag":"markdown",
-                                "content":"**时间：**\n2024-3-11 至 2024-3-15 （共 5 天）",
-                                "text_align":"left",
-                                "text_size":"normal",
-                                "icon":{
-                                   "tag":"standard_icon",
-                                   "token":"time_outlined"
+                    "margin": "16px 0px 0px 0px"
+                },
+                {
+                    "tag": "hr"
+                },
+                {
+                    "tag": "markdown",
+                    "content": "**备注：**\n世界这么大，我想去转转",
+                    "text_align": "left",
+                    "text_size": "normal",
+                    "icon": {
+                        "tag": "standard_icon",
+                        "token": "driveload_outlined"
+                    }
+                },
+                {
+                    "tag": "column_set",
+                    "flex_mode": "stretch",
+                    "background_style": "default",
+                    "horizontal_spacing": "8px",
+                    "horizontal_align": "left",
+                    "columns": [
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "vertical_align": "top",
+                            "vertical_spacing": "8px",
+                            "background_style": "default",
+                            "elements": [
+                                {
+                                    "tag": "markdown",
+                                    "content": "**备注：**\n世界这么大，我想去转转",
+                                    "text_align": "left",
+                                    "text_size": "normal",
+                                    "icon": {
+                                        "tag": "standard_icon",
+                                        "token": "readinfo_outlined"
+                                    }
                                 }
-                             }
-                          ],
-                          "weight":1
-                       },
-                       {
-                          "tag":"column",
-                          "width":"weighted",
-                          "vertical_align":"top",
-                          "vertical_spacing":"8px",
-                          "background_style":"default",
-                          "elements":[
-                             {
-                                "tag":"markdown",
-                                "content":"**备注：**\n世界这么大，我想去转转",
-                                "text_align":"left",
-                                "text_size":"normal",
-                                "icon":{
-                                   "tag":"standard_icon",
-                                   "token":"readinfo_outlined"
-                                }
-                             }
-                          ],
-                          "weight":1
-                       }
+                            ],
+                            "weight": 1
+                        }
                     ],
-                    "margin":"16px 0px 0px 0px"
-                 },
-                 {
-                    "tag":"hr"
-                 },
-                 {
-                    "tag":"action",
-                    "actions":[
-                       {
-                          "tag":"button",
-                          "text":{
-                             "tag":"plain_text",
-                             "content":"按xi钮"
-                          },
-                          "type":"danger_filled",
-                          "complex_interaction":true,
-                          "width":"fill",
-                          "size":"medium",
-                          "icon":{
-                             "tag":"standard_icon",
-                             "token":"apaas_colorful"
-                          },
-                          "multi_url":{
-                             "url":"baidu.,com",
-                             "pc_url":"",
-                             "ios_url":"",
-                             "android_url":""
-                          }
-                       }
+                    "margin": "16px 0px 0px 0px"
+                },
+                {
+                    "tag": "hr"
+                },
+                {
+                    "tag": "note",
+                    "elements": [
+                        {
+                            "tag": "standard_icon",
+                            "token": "emoji_outlined"
+                        },
+                        {
+                            "tag": "plain_text",
+                            "content": "文本内容"
+                        }
                     ]
-                 }
-              ]
-           },
-           "i18n_header":{
-              "zh_cn":{
-                 "title":{
-                    "tag":"plain_text",
-                    "content":"GIT推送事件 "
-                 },
-                 "subtitle":{
-                    "tag":"plain_text",
-                    "content":""
-                 },
-                 "template":"blue",
-                 "ud_icon":{
-                    "tag":"standard_icon",
-                    "token":"todo_colorful"
-                 }
-              }
-           }
-        },
-        "variables":[
-           
-        ]
-    };
+                }
+            ],
+            "header": {
+                "title": {
+                    "tag": "plain_text",
+                    "content": "GITEA 事件触发"
+                },
+                "subtitle": {
+                    "tag": "plain_text",
+                    "content": ""
+                },
+                "template": "blue",
+                "ud_icon": {
+                    "tag": "standard_icon",
+                    "token": "todo_colorful"
+                }
+            }
+        }
+    }
+
 
 /***/ }),
 
